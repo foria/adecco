3f44bed7860902de049fb7c2db3aa552466e5d90
Marco Melluso <foriaa@gmail.com>
2019-04-11 18:05:12 +0800

    moved to bootstrap 3, header fixed, 100% height of 1st section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,19 @@ $(document).ready(function(){
         $(this).parent().remove();
     } )
 
+    $('input, textarea').focus(function() {
+        $(this).parents('.form-group').addClass('onfocus');
+    });
+
+    $('input[type="radio"]').on('click', function() {
+        $(this).parents('.form-group').addClass('onfocus');
+    });
+
+    $('input, textarea').focusout(function() {
+        if($(this).val().length > 0){
+            $(this).parents('.form-group').addClass('filled');
+        }
+        $(this).parents('.form-group').removeClass('onfocus');
+    });
+
 })
